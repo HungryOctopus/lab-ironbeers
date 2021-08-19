2e39824dcd2cc260f266d1db8f39ca763f7887f0
Amélie Monnoyer <49373560+HungryOctopus@users.noreply.github.com>
2021-08-19 20:23:32 +0200

    done until iteration 3.1

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+// To tell hbs where partial templates are located
+hbs.registerPartials(__dirname + '/views/partials');
+
+// Express needs to know which templating engine to use
+// We need to set the option 'view engine' to the name of the package
+// that will render our views
 app.set('view engine', 'hbs');
+// We need to tell express where to look up our view templates
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -20,6 +27,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers.hbs', {
+        beers: beersFromApi
+      });
+      console.log('Beers from the database: ', beersFromApi);
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  res.render('random');
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
